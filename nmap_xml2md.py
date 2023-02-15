c564af7746de71bb4f520b55f594db9447a82c38
0xalpaca <0xalpaca@gmail.com>
2023-02-15 19:46:19 +0100

    Update

--- nmap_xml2md.py
+++ nmap_xml2md.py
@@ -12,49 +12,63 @@ def main():
         epilog="Credit: 0xalpaca",
     )
     parser.add_argument('filename', help="must be an xml file")
+    parser.add_argument('output_folder', help="this is the folder where the script will save the files and folders")
 
     args = parser.parse_args()
-    if not args.filename:
-        parser.print_help()
-    elif not os.path.exists(args.filename):
+    if not os.path.exists(args.filename):
         print("File does not exist")
     else:
-        parse_nmap_xml(args.filename)
+        basepath = args.output_folder
+        parse_nmap_xml(args.filename, basepath)
 
 
-def parse_nmap_xml(xml_file):
+def parse_nmap_xml(xml_file, basepath):
     tree = ElementTree.parse(xml_file)
     root = tree.getroot()
-    for host in root.findall('host'):
-        ip = host.find('address').get('addr')
-        os.makedirs(ip)
 
-        print(f"[+] Created folder {ip}")
+    if not os.path.exists(basepath):
+        os.makedirs(basepath)
 
-        with open(os.path.join(ip, ip + ".md"), 'a') as fi:
+    with open(os.path.join(basepath, basepath + ".md"), 'w') as fb:
 
-            fi.write(">[!info] Open ports\n")
+        fb.write(">[!info] Scanned IPs list\n")
 
-            for port in host.findall('ports/port'):
-                port_number = port.get('portid')
-                protocol = port.get('protocol')
+        for host in root.findall('host'):
+            ip = host.find('address').get('addr')
+            os.makedirs(basepath + "/" + ip)
 
-                fi.write(">- [[" + protocol + "-" + port_number + "]]\n")
+            fb.write(">- [[" + ip + "]]\n")
 
-                with open(os.path.join(ip, protocol + "-" + port_number + ".md"), 'w') as fp:
-                    fp.write("```nmap\n")
-                    for element in port.iter():
-                        if element.tag == "state":
-                            fp.write(f"State: {element.get('state')}\n")
-                            fp.write(f"{element.get('reason')}\n")
-                        elif element.tag == "service":
-                            fp.write(f"Service: {element.get('name')}\n")
-                            fp.write(f"Product: {element.get('product')}\n")
-                            fp.write(f"Version: {element.get('version')}\n")
-                        elif element.tag == "script":
-                            fp.write(f"Script: {element.get('id')}\n")
-                            fp.write(f"Output: {element.get('output')}\n")
-                    fp.write("```\n")
+            print(f"[+] Created folder {ip}")
+
+            with open(os.path.join(basepath, ip, ip + ".md"), 'w') as fi:
+
+                fi.write(">[!info] Open ports\n")
+
+                for port in host.findall('ports/port'):
+                    port_number = port.get('portid')
+                    protocol = port.get('protocol')
+
+                    fi.write(">- [[" + protocol + "-" + port_number + "]]\n")
+
+                    with open(os.path.join(basepath, ip, protocol + "-" + port_number + ".md"), 'w') as fp:
+                        fp.write("```nmap\n")
+                        for element in port.iter():
+                            if element.tag == "state":
+                                state = element.get('state')
+                                fp.write(f"State: {state}\n")
+                                fp.write(f"{element.get('reason')}\n")
+                            elif element.tag == "service":
+                                fp.write(f"Service: {element.get('name')}\n")
+                                fp.write(f"Product: {element.get('product')}\n")
+                                fp.write(f"Version: {element.get('version')}\n")
+                            elif element.tag == "script":
+                                fp.write(f"Script: {element.get('id')}\n")
+                                fp.write(f"Output: {element.get('output')}\n")
+                        fp.write("```\n")
+                        fp.write(f"\n---\n\n#nmap #{protocol}/{port_number} #state/{state}\n")
+                fi.write("\n")
+        fb.write("\n")
 
 
 if __name__ == '__main__':
